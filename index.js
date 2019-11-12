b75602d434449d157a87ebabbbc0313bda0b7e45
Kris! <42949874+krstphrrr@users.noreply.github.com>
2019-11-12 07:58:14 -0700

    js

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+
+
+
+// MIDDLEWAREZ
+// body parser - middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+// set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log(`escuchando en..${PORT}`);
 })
 
+
 app.use('/api/items', require('./routes/api/items'));
-// get :

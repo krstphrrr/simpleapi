2de6710f2fabe81ded5882d43624b20f63042640
Kris! <42949874+krstphrrr@users.noreply.github.com>
2019-11-30 10:19:04 -0700

    .

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const exphbs = require('express-handlebars')
 
-const { Client } = require('pg');
+const Client = require('pg');
 
 // database
 const db = require('./config/database');
@@ -32,36 +33,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`escuchando en..${PORT}`);
 })
-
-
-
-
-
-
-// // MIDDLEw
-// // body parser - middleware
-// app.use(express.json());
-// app.use(express.urlencoded({extended: false}))
-
-// set static folder
-
-
-// app.use('/', (req,res,next)=>{
-//     res.status(200).send('ok')
-// })
-
-
-// // app.use('/api/items', require('./routes/api/items'));
-// // // items router to modularize the whole thing
-// // const itemsRouter = express.Router()
-// // app.use('/items', itemsRouter)
-
-
-// const client = new Client(conf.connStr())
-
-// client.connect()
-
-// client.query('SELECT * FROM iris', (err,res)=>{
-//     console.log(err,res)
-//     client.end()
-// })

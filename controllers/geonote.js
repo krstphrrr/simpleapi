f7a7505ebaf6c0ffb90d238a646588cf0a1b91a1
Kris! <42949874+krstphrrr@users.noreply.github.com>
2019-12-30 16:12:25 -0700

    edits objects a-ok!

--- controllers/geonote.js
+++ controllers/geonote.js
@@ -1,10 +1,10 @@
 const Geonote = require('../models/geonote')
 
+// get all the notes in db
+
 exports.getGeonote = (req, res, next) => {
-    // const point = { type: 'Point', coordinates: [req.body.long, req.body.lat] }
     Geonote.findAll()
         .then(geonote =>{
-            console.log(geonote[0].dataValues.geom)
             res.render('geo',{
                 items: geonote,
                 latlong: geonote[0].dataValues.geom.coordinates,
@@ -16,6 +16,8 @@ exports.getGeonote = (req, res, next) => {
             console.log(err)
         })
 }
+
+//get a single note from db
 
 exports.getGeo = (req, res, next) => {
   const geoId = req.params.id
@@ -32,13 +34,25 @@ exports.getGeo = (req, res, next) => {
     });
 };
 
-exports.addGeonote = (req, res, next)=>{
+// get page to ADD a new note to db 
+
+exports.getaddGeonote = (req, res, next)=>{
+    res.render('edit-geo', {
+        pageTitle:'add note',
+        editing:false
+    })
+}
+
+// actually ADD the new note to db
+
+exports.postaddGeonote = (req, res, next)=>{
     const geom = req.body.geom;
     const txt = req.body.txt;
     const userid = req.body.userid
-    // const rest = (...args)=>{return `${args}`}
-    // const point = {type: 'Point', coordinates:rest(geom).split(",")};
-    const point = { type: 'Point', coordinates: [req.body.long, req.body.lat] };
+    const rest = (...args)=>{return `${args}`}
+    const point = {type: 'Point', coordinates:rest(geom).split(",")};
+    // const point = { type: 'Point', coordinates: [req.body.long, req.body.lat] };
+    // ^^^could work but needs update in pug: individual inputforms that reference lat & long
     Geonote.create({
          geom: point,
          txt: txt,
@@ -54,7 +68,67 @@ exports.addGeonote = (req, res, next)=>{
     })
 }
 
+// get page to UPDATE a particular note in db
+
+exports.editGeo = (req, res, next) => {
+  const geoId = req.params.id;
+  const editMode = req.query.edit
+  if(!editMode){
+      console.log(req)
+      return res.redirect('/')
+  }
+  
+  Geonote.findByPk(geoId)
+    .then(geonote => {
+    if (!geonote) {
+        console.log("segunda salida")
+        return res.redirect('/')
+    }
+      res.render("edit-geo", {
+        items: geonote,
+        pageTitle: "GEO!!",
+        path: "/edit-geo",
+        editing: true,
+        geoId: geoId
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+// actually POST updated geonote to db
+
+exports.posteditGeo = (req, res, next) => {
+  const geoId = req.body.nid
+  const updatedgeom = req.body.geom;
+  const updatedtxt = req.body.txt;
+  const updateduserid = req.body.userid;
+  const rest = (...args) => {
+    return `${args}`;
+  };
+  const point = { type: "Point", coordinates: rest(updatedgeom).split(",") };
+  Geonote.findByPk(geoId)
+    .then(geonote => {
+      geonote.geom = point
+      geonote.txt = updatedtxt;
+      geonote.userid = updateduserid
+      return geonote.save()
+    })
+    .then(result => {
+      console.log("updatedddd");
+      res.redirect("/geo");
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect("/geo");
+    });
+};
+
+
 exports.getIndex = (req, res, next) =>{
-    res.render('index')
+    res.render('index', {
+        editing: false
+    })
         
 }
--- routes/geo.js
+++ routes/geo.js
@@ -6,7 +6,11 @@ const geoController = require('../controllers/geonote')
 const router = express.Router()
 
 router.get("/geo",geoController.getGeonote)
-router.post("/", geoController.addGeonote)
+router.post("/add-geo", geoController.postaddGeonote)
+router.get("/add-geo", geoController.getaddGeonote)
 router.get("/", geoController.getIndex)
 router.get("/geo/:id", geoController.getGeo);
+router.get("/edit-geo/:id", geoController.editGeo)
+router.post("/edit-geo", geoController.posteditGeo)
+
 module.exports = router

8e372e3da64718920ed567f9aafc2cbcb2f8d6b7
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-01-01 23:43:57 -0700

    clientside private / public locations a-ok!

--- modelgen.js
+++
@@ -1,7 +0,0 @@
-
-const lst = [3,4,4]
-const geom = (...args) => {
-  // args = args.toString()
-  return `${args}`
-};
-console.log(geom(lst).split(","))
--- public/scripts/mp.js
+++ public/scripts/mp.js
@@ -2,4 +2,3 @@ const str = () => {
   return "pk.eyJ1Ijoia3JzdHBocnJyIiwiYSI6ImNrNHU1cGEybjA4aGszam53azdlNnZhZTQifQ.BdC-b5iEOIRkQSc5rpTB-A";
 };
 
-exports.str = str;

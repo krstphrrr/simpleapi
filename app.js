039df21f327c1f7ada580c63f8b6da4768c83749
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-01-05 20:49:17 -0700

    added password recovery

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Sequelize = require('sequelize');
 const session = require('express-session')
 const pgDBstore = require('connect-session-sequelize')(session.Store)
 const csrf = require('csurf')
+const flash = require('connect-flash')
 
 // database
 const db = require('./config/database');
@@ -42,6 +43,8 @@ app.use(
   })
 );
 app.use(csrfProtect)
+app.use(flash())
+
 
 app.use((req, res, next)=>{
   if(!req.session.user){
